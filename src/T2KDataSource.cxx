2ee24fbc56ab6649ca8a93069b36bbd1fd6a75ce
David Hadley <d.r.hadley@warwick.ac.uk>
2018-12-04 05:14:38 +0900

    Move method definitions from header to source file.

--- src/T2KDataSource.cxx
+++ src/T2KDataSource.cxx
@@ -168,9 +168,9 @@ namespace T2K {
     }
 
     T2KDataFrameWrapper MakeT2KDataFrame(std::vector<std::string> fileNameGlob,
-                                                bool enabletruth,
-                                                std::vector<std::string> treeNames,
-                                                BunchTiming bunchTiming) {
+                                         bool enabletruth,
+                                         std::vector<std::string> treeNames,
+                                         BunchTiming bunchTiming) {
         if(!enabletruth) {
             // Remove truth trees
             std::vector<std::string> t;
@@ -201,10 +201,10 @@ namespace T2K {
 #ifdef T2K_USE_GENIE
                     reco.Define("t2kroovtx", T2K::tcatorvec<ND::GRooTrackerVtx*>, {"GRooTrackerVtx_Vtx"})
 #else
-                    reco.Define("t2kroovtx", T2K::tcatorvec<ND::GRooTrackerVtx*>, {"NRooTrackerVtx_Vtx"})
+                                    reco.Define("t2kroovtx", T2K::tcatorvec<ND::GRooTrackerVtx*>, {"NRooTrackerVtx_Vtx"})
 #endif
-                    .Define("t2ktruthtraj", T2K::tcatorvec<TruthTraj*>, {"Trajectories_Trajectories"})
-                    .Define("t2ktruthvtx", T2K::tcatorvec<TruthVtx*>, {"Vertices_Vertices"});
+                            .Define("t2ktruthtraj", T2K::tcatorvec<TruthTraj*>, {"Trajectories_Trajectories"})
+                            .Define("t2ktruthvtx", T2K::tcatorvec<TruthVtx*>, {"Vertices_Vertices"});
         }
         else {
             // No truth, fill with empty containers
@@ -215,6 +215,185 @@ namespace T2K {
         // Wrap and return result
         return T2KDataFrameWrapper(std::move(tdf), std::move(reco));
     }
+
+
+    ULong64_t T2KDataSource::GetEntries() { return _chainreaders.at(0)->GetEntries() * nbunches; }
+
+    int T2KDataSource::GetNSlots() { return _nslots; }
+
+    TChain& T2KDataSource::GetSlotChain(int slot, int treeindex) { return _chainreaders.at(treeindex)->GetSlotChain(slot); }
+
+    std::vector<void *> T2KDataSource::GetColumnReadersImpl(std::string_view name, const std::type_info &t) {
+        return _chainreaders.at(_columnnamemap.at(std::string(name)))->GetColumnReadersImpl(getBranchName(name), t);
+    }
+
+
+    T2KDataSource::T2KDataSource(std::vector<std::string> treenames_, std::vector<std::string> filename, int nbunches_) :
+            treenames(treenames_),
+            processed(0), nbunches(nbunches_) {
+        for (auto &tn : treenames) {
+            _chainreaders.push_back(make_unique<T2K::MyRRootDS>(tn, filename));
+        }
+    }
+
+    std::string T2KDataSource::GetTypeName(std::string_view colName) const {
+        return _chainreaders.at(_columnnamemap.at(std::string(colName)))->GetTypeName(getBranchName(colName));
+    }
+
+    const std::vector<std::string>& T2KDataSource::GetColumnNames() const {
+        if (columnnames.size() == 0) {
+            for (int i = 0; i < treenames.size(); ++i) {
+                auto tn = treenames.at(i);
+                auto &chr = _chainreaders.at(i);
+                for (auto &c : chr->GetColumnNames()) {
+                    auto s = tn.substr(tn.find("/") + 1);
+                    if (c == "CTMTriggerPattern" || c == "SoftwareVersion" || c == "GeometryHash" ||
+                        c == "AlignmentId") {
+                        std::cout << "Skipping" << c << std::endl;
+                        continue;
+                    }
+                    std::string colname = s + "_" + c;
+                    columnnames.push_back(colname);
+                    if (_columnnamemap.insert({colname, i}).second == false) {
+                        // insertion failed! Duplicate column names is not allowed
+                        throw std::runtime_error("Duplicate column names " + colname + " is not allowed");
+                    }
+
+                }
+            }
+        }
+        return columnnames;
+    }
+
+    std::string T2KDataSource::getBranchName(std::string colName) const {
+        return colName.substr(colName.find("_") + 1);
+    }
+
+    std::string T2KDataSource::getBranchName(std::string_view colName) const {
+        return getBranchName(std::string(colName));
+    }
+
+    bool T2KDataSource::HasColumn(std::string_view colName) const {
+        for (auto &n : columnnames) {
+            if (n == colName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void T2KDataSource::InitSlot(unsigned int slot, ULong64_t firstEntry) {
+        for (auto &c : _chainreaders) { c->InitSlot(slot, firstEntry); }
+    }
+
+    void T2KDataSource::Initialise() {
+        for (auto &c : _chainreaders) { c->Initialise(); }
+    }
+
+    std::vector<std::pair<ULong64_t, ULong64_t> > T2KDataSource::GetEntryRanges() {
+        for (auto &c : _chainreaders) { c->GetEntryRanges(); }
+        std::vector<std::pair<ULong64_t, ULong64_t> > result;
+        //auto nentries = nbunches*1000;//_chainreaders.front()->GetEntries();
+        auto nentries = nbunches * _chainreaders.front()->GetEntries();
+        if (processed < nentries) {
+            auto chunk = nentries / _nslots;
+            auto start = processed;
+            auto end = start;
+            for (unsigned int i = 0; i < _nslots; ++i) {
+                start = end;
+                end += start + chunk;
+                result.push_back({start, end});
+            }
+            result.back().second = nentries;
+            processed = result.back().second;
+        }
+        return result;
+    }
+
+    bool T2KDataSource::SetEntry(unsigned int slot, ULong64_t entry) {
+        bool result = true;
+        for (auto &reader : _chainreaders) {
+            result = result && reader->SetEntry(slot, GetEntryNumber(entry));
+        }
+        return result;
+    }
+
+    void T2KDataSource::SetNSlots(unsigned int nSlots) {
+        _nslots = nSlots;
+        for (auto &chr : _chainreaders) {
+            chr->SetNSlots(nSlots);
+        }
+    }
+
+    void T2KDataSource::Finalise() {
+        for (auto &c : _chainreaders) { c->Finalise(); }
+    }
+
+    void T2KDataSource::FinaliseSlot(unsigned int i) { for (auto &c : _chainreaders) { c->FinaliseSlot(i); }}
+
+    ULong64_t T2KDataSource::GetEntryNumber(ULong64_t entry) { return entry / this->nbunches; }
+
+    ULong64_t T2KDataSource::GetBunchNumber(ULong64_t entry) { return entry % this->nbunches; }
+
+    BunchTiming::BunchTiming(ROOT::VecOps::RVec<double> centres, double window) {
+        _data.emplace_back(Record{numeric_limits<int>::min(), numeric_limits<int>::max(), centres, window});
+    }
+
+    BunchTiming::BunchTiming(ROOT::VecOps::RVec<double> lowedges, ROOT::VecOps::RVec<double> highedges) {
+        _data.emplace_back(Record{numeric_limits<int>::min(), numeric_limits<int>::max(), lowedges, highedges});
+    }
+
+    BunchTiming::BunchTiming(double window) {
+        // Taken from:
+        //  https://repo.nd280.org/viewvc/ND280/psyche/psycheND280Utils/data/BunchPosition.dat?view=log
+        _data = ROOT::VecOps::RVec<Record>{
+                // moved below: {-1,      -1,   {2750.2,  3332.0,  3914.7,  4497.0,  5078.4,  5659.7,  6243.4,  6824.2}, window},
+                {0,                          6000,                       {2839.7,  3423.5,  4005.4,  4588.6,  5172.2,  5754.6,  -1000.0, -1000.0}, window},
+                {6000,                       7000,                       {2853.95, 3444.15, 4030.41, 4620.34, 5180.28, 5770.12, 6343.77, 6924.67}, window},
+                {7000,                       8000,                       {3019.11, 3597.74, 4180.73, 4763.93, 5346.49, 5927.83, 6508.5,  7093.56}, window},
+                {8000,                       8550,                       {3024.22, 3606.11, 4188.01, 4769.9,  5351.79, 5933.68, 6515.58, 7097.47}, window},
+                {8550,                       8800,                       {3013.55, 3597.55, 4178.24, 4758.78, 5338.21, 5927.31, 6505.71, 7086.80}, window},
+                {8895,                       9300,                       {3014.69, 3600.57, 4178.89, 4764.24, 5342.35, 5931.73, 6506.37, 7090.15}, window},
+                {9301,                       9999,                       {3014.69, 3600.57, 4178.89, 4764.24, 5342.35, 5931.73, 6506.37, 7090.15}, window},
+                {10251,                      10522,                      {3013.71, 3595.50, 4178.37, 4764.48, 5342.07, 5922.57, 6509.16, 7086.23}, window},
+                {10826,                      12063,                      {3018.20, 3599.10, 4184.62, 4769.22, 5345.73, 5932.20, 6511.85, 7096.31}, window},
+                {12064,                      99999,                      {2994.20, 3581.27, 4165.74, 4752.16, 5335.15, 5917.88, 6502.08, 7085.51}, window},
+                {numeric_limits<int>::min(), numeric_limits<int>::max(), {2750.2,  3332.0,  3914.7,  4497.0,  5078.4,  5659.7,  6243.4,  6824.2},  window},
+
+        };
+    }
+
+    int BunchTiming::size() const { return _data.front().timelow.size(); }
+
+    double BunchTiming::low(int bunch, int run) const {
+        for (const Record &r : this->_data) {
+            if (r.runlow <= run && run < r.runhigh) {
+                return r.timelow.at(bunch);
+            }
+        }
+        throw std::runtime_error("unknown run number");
+    }
+
+    double BunchTiming::high(int bunch, int run) const {
+        for (const Record &r : this->_data) {
+            if (r.runlow <= run && run < r.runhigh) {
+                return r.timehigh.at(bunch);
+            }
+        }
+        throw std::runtime_error("unknown run number");
+    }
+
+    double BunchTiming::center(int bunch, int run) const { return (low(bunch, run) + high(bunch, run)) / 2.0; }
+
+    ROOT::VecOps::RVec<double> BunchTiming::allbunchcentres(int run) const {
+        for (const Record &r : this->_data) {
+            if (r.runlow <= run && run < r.runhigh) {
+                return r.timecenter;
+            }
+        }
+        throw std::runtime_error("unknown run number");
+    }
+
 
 }
 
